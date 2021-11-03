9fd5e758beea37cb84c814a10618a2754e57aefc
Jinyu Wang <ellie5201314@gmail.com>
2021-11-03 19:21:28 -0400

    Initial Commit

---
+++ MoneyManager/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MoneyManager
+//
+//  Created by Yu Jin on 10/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    @IBAction func Login(_ sender: Any) {
+    }
+    
+    @IBAction func Update(_ sender: Any) {
+    }
+    @IBOutlet weak var LinktoSpending: UIButton!
+    
+    @IBOutlet weak var LinktoIncome: UIButton!
+}
+
